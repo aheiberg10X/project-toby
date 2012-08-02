48dacd036c7d55b56876d0772f5689eb7702f7c2
andrew <andrew@rodion.(none)>
2012-08-01 23:57:17 -0700

    code to do the rollout simulation for an arbitrary num of players

--- deck.py
+++ deck.py
@@ -3,26 +3,47 @@ from random import sample
 
 class Deck:
     def __init__(self) :
-        self.cards = range(52)
+        self.cards = set(range(52))
         self.suits = ('h','d','c','s')
         #human-readable values for the 52 cards
         #matches that of pypoker-eval
         self.humancards = []
         for suit in self.suits :
-            for num in [str(v) for v in range(2,11)] + ['J','Q','K','A'] :
+            for num in [str(v) for v in range(2,10)] + ['T','J','Q','K','A'] :
                 self.humancards.append( str(num)+suit )
 
-    #draw n from the cards remaining in the deck
+    def shuffle(self) :
+        self.cards = set(range(52))
+
+    #if all the cards are present to be be removed, do so and return True
+    #if not, do not remove anything and return False
+    def remove( self, cards ) :
+        try :
+            ix = 0
+            for c in cards : 
+                self.cards.remove(c)
+                ix += 1
+            return True
+        except KeyError as ve :
+            #put back the removed cards
+            for i in range(ix) :
+                self.cards.add(cards[i])
+            return False
+
+    #since d.cards is a set don't need to worry about duplicates
+    def replace( self, cards ) :
+        for c in cards : self.cards.add(c)
+
+    #draw n at random from the cards remaining in the deck
     def draw( self, n ) :
         selections = sample(self.cards, n)
-        for s in selections :
-            self.cards.remove(s)
+        self.remove( selections )
         return selections
         #return tuple( self.humancards[s] for s in selections )    
 
     def getCardinality( self, card ) :
         if type(card) == int :
-            return card/4+1
+            return (card % 13)+2
         elif type(card) == str :
             return card[0]
         else :
@@ -30,7 +51,7 @@ class Deck:
 
     def getSuit( self, card ) :
         if type(card) == int :
-            return self.suits[card % 4]
+            return self.suits[card / 13]
         elif type(card) == str :
             return card[1]
         else :
@@ -40,17 +61,50 @@ class Deck:
     def makeHuman( self, cards ) :
         return [self.humancards[c] for c in cards]
 
+    def stringifyCardinality( self, c ) :
+        d = {2:'2',3:'3',4:'4',5:'5',6:'6',7:'7',8:'8',9:'9', \
+             10:'T',11:'J',12:'Q',13:'K',14:'A'}
+        return d[c]
+
+    #return the distinct type of hole card (1326 -> 169)
+    #hole_cards are [int,int]
+    def collapseHC( self, hole_cards ) :
+        c1 = self.getCardinality(hole_cards[0])
+        c2 = self.getCardinality(hole_cards[1])
+        s1 = self.getSuit(hole_cards[0])
+        s2 = self.getSuit(hole_cards[1])
+        if s1 == s2 :
+            suit = 's'
+        else:
+            suit = 'o'
+
+        if c1 < c2 :
+            return ('%s%s%s' % (self.stringifyCardinality(c1), \
+                                self.stringifyCardinality(c2), \
+                                suit) )
+        elif c1 == c2 :
+            return ('%s%s' % (self.stringifyCardinality(c1), \
+                              self.stringifyCardinality(c2)))
+        else :
+            return ('%s%s%s' % (self.stringifyCardinality(c1), \
+                                self.stringifyCardinality(c2),
+                                suit) )
+
+
+
 def main() :
     d = Deck()
-    for i in range(52) : 
-        print i
-        print d.humancards[i]
+    d.remove([1,2,3])
+    d.remove([4,1])
+
+    print d.collapseHC([25,51])
+    print d.cards
     #print d.humancards
-    drawn = d.draw(3)
-    print drawn
-    print d.getCardinality(drawn[0])
-    print d.getSuit(drawn[0])
-    print d.humancards.index(drawn[0])
+    #drawn = d.draw(3)
+    #print drawn
+    #print d.getCardinality(drawn[0])
+    #print d.getSuit(drawn[0])
+    #print d.humancards.index(drawn[0])
 
 if __name__ == '__main__' :
     main()
--- rollout.py
+++ rollout.py
@@ -1,15 +1,71 @@
-from itertools import combinations
+from itertools import combinations, product
 from deck import Deck
 import pokereval
 
+#hole card rollout for arbitrary number of players
+
+game = "holdem"
 pe = pokereval.PokerEval()
 d = Deck()
-count = 0
-hole_cards = list(d.draw(2))
-print "Hole Cards", d.makeHuman(hole_cards)
 
-for board in combinations(d.cards,5) :
-    print "Board,", d.makeHuman(board)
-    print "Eval,", pe.best( "hi", hand=hole_cards + list(board) )
+num_players = 2
+#some of these aren't valid, e.g [(0,1),(0,1)]
+#but the impossible ones are excluded later
+all_hole_cards = product( combinations(d.cards,2), repeat=num_players )
+
+count = 0
+
+#five wildcards
+board = ["__"]*5
+
+#store the accumlated EV's for each of the 169 distinct HCs
+results = {}
+for hole_cards in all_hole_cards :
+    #start with a new deck each time
+    d.shuffle()
+
+    #if deck.remove() lets us take out each pair of hole cards
+    #then the assignment is valid one
+    valid = all([d.remove(hc) for hc in hole_cards])
+    if valid :
+        #just turn it into lists for poker_eval
+        hole_cards = [list(hc) for hc in hole_cards]
+        print hole_cards
+        #do the exhaustive rollout simulation
+        r = pe.poker_eval( game, hole_cards, dead=["__"], board=board )
+
+        #add the EV's up
+        for hc,evl in zip(hole_cards,r["eval"]) :
+            EV = evl["ev"]
+            dhc = d.collapseHC( hc )
+            if dhc in results :
+                results[dhc] += EV 
+            else :
+                results[dhc] = 0
+        print "\n"
+    
     count += 1
-    if count > 10 : break
+    if count > 300 : break
+
+print 'adsfasdfadsf'
+for r in results :
+    print r, results[r]
+
+
+#a = list(d.draw(2))
+#b = list(d.draw(2))
+#print "Player A - Hole Cards", d.makeHuman(a)
+#print "Player B - Hold Cards", d.makeHuman(b)
+#
+#num_known_board = 3
+#for board in combinations(d.cards, num_known_board) :
+    #pockets = [a,b]
+    #board = d.makeHuman(board) + ["__"] * (5-num_known_board)
+    #print "Board,", board
+    #print r["info"]
+    #for pr in r["eval"] :
+        #print pr["ev"]
+#
+    #print "\n\n"
+    #count += 1
+    #if count > 10 : break
