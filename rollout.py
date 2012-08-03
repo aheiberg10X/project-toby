7c63ae98a25d46a0daaf75bc2fe8d846c811e027
andrew <andrew@rodion.(none)>
2012-08-03 15:08:44 -0700

    parallel

--- rollout.py
+++ rollout.py
@@ -1,30 +1,24 @@
 from itertools import combinations, product
 from deck import Deck
 import pokereval
+from multiprocessing import Process, Queue, Pool
+from time import time
 
-#hole card rollout for arbitrary number of players
 
-game = "holdem"
-pe = pokereval.PokerEval()
-d = Deck()
+def square(x) : 
+    return x*x
 
-num_players = 2
-#some hole card assignments will be impossible
-#e.g 2c2d 2c7h
-#but these impossible ones are excluded later by deck.remove below
-all_hole_cards = combinations( combinations(d.cards,2), num_players )
-
-count = 0
-
-#five wildcards
-board = ["__"]*5
-
-#store the accumlated EV's for each of the 169 distinct HCs
-#[acc_ev,num_trials]
-results = {}
-for hole_cards in all_hole_cards :
-    #start with a new deck each time
-    d.shuffle()
+def rollout( hole_cards ) :
+    game = "holdem"
+    #five wildcards
+    num_known_board = 0
+    board = ["__"]*(5-num_known_board)
+    d = Deck()
+    pe = pokereval.PokerEval()
+    #store the accumlated EV's for each of the 169 distinct HCs
+    #[acc_ev,num_trials]
+    results = {}
+    count = 0
 
     #if deck.remove() lets us take out each pair of hole cards
     #then the assignment is valid one
@@ -32,35 +26,90 @@ for hole_cards in all_hole_cards :
     if valid :
         #just turn it into lists for poker_eval
         hole_cards = [list(hc) for hc in hole_cards]
-        print hole_cards
         #do the exhaustive rollout simulation
-        r = pe.poker_eval( game, hole_cards, dead=["__"], board=board )
+        r = pe.poker_eval( game, \
+                           hole_cards, \
+                           dead=["__"]*3, \
+                           board=board) 
+                           #iterations = 500000)
 
         #add the EV's up
         for hc,evl in zip(hole_cards,r["eval"]) :
             EV = evl["ev"]
             dhc = d.collapseHC( hc )
             if dhc in results :
-                results[dhc][0] += EV 
-                results[dhc][1] += 1
+                results[dhc] += EV 
                 
             else :
-                results[dhc] = []
-                results[dhc].append( EV )
-                results[dhc].append( 1 )
+                results[dhc] = EV
 
         print "\n"
     
-    count += 1
-    #if count > 300 : break
+    #count += 1
+    #if count > 100 : break
 
-print 'adsfasdfadsf'
-fresults = open( "%s_player_rollout.txt" % num_players, 'w' )
-for r in results :
-    print r, results[r]
-    fresults.write( "%s, %s" % (r,results[r]) )
-fresults.close()
+    return results
 
+#hole card rollout for arbitrary number of players
+def main() :
+    num_threads = 4 
+    
+    num_players = 2
+    d = Deck()
+    #some hole card assignments will be impossible
+    #e.g 2c2d 2c7h
+    #but these impossible ones are excluded later by deck.remove below
+    all_hole_cards = combinations( combinations(d.cards,2), num_players )
+
+    #test = []
+    #for i in range(3000) :
+        #test.append( all_hole_cards.next() )
+
+    results={}
+    #def callback( hand_scores ) :
+        #for hand in hand_scores :
+            #if hand in results :
+                #results[hand] += hand_scores[hand]
+            #else :
+                #results[hand] = hand_scores[hand]
+
+
+    #map
+    p = Pool(processes=num_threads)
+    a = time()
+    r = p.map( rollout, all_hole_cards )
+    b  = time()
+    
+    #reduce
+    c = time()
+    for d in r :
+        for hand in d :
+            if hand in results :
+                results[hand][0] += d[hand]
+                results[hand][1] += 1
+            else :
+                results[hand] = []
+                results[hand].append( d[hand] )
+                results[hand].append( 1 )
+    d = time()
+
+    print results
+    p.close()
+    p.join()
+
+    fresults = open( "%s_player_rollout.txt" % num_players, 'w' )
+    fresults.write("mapping time: %s\n" % str(b-a) )
+    fresults.write("reduce time: %s\n" % str(d-c) )
+    for r in results :
+        #print r, results[r]
+        fresults.write( "%s, %s\n" % (r,results[r]) )
+    fresults.close()
+
+
+
+if __name__ == "__main__" :
+    #print rollout( ((0,1),(4,5)) )
+    main()
 
 #a = list(d.draw(2))
 #b = list(d.draw(2))
