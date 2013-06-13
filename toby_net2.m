97b06325594dd20ec82f392ba3f1efc5ee0bc5c4
andrew <andrew@rodion.(none)>
2013-06-13 15:32:48 -0700

    some tweaks to toby_*

--- toby_net2.m
+++ toby_net2.m
@@ -2,7 +2,8 @@ seed = 42;
 randn('state',seed);
 N = 12;
 
-training = csvread('../../project-toby/nodes/hugh_SartreNL/perm1/training_4-rounds_showdown.csv') %show_4-round_perm0_train_merged_scaled.csv');
+training = csvread('../../project-toby/nodes/hugh_SartreNL/perm1/training_4-rounds_showdown.csv'); %show_4-round_perm0_train_merged_scaled.csv');
+evidence = training(:,1:N)';
 
 dag = zeros(N,N);
 
@@ -29,11 +30,16 @@ discrete_nodes = 1:12;
 
 bnet = mk_bnet( dag, node_sizes, 'observed', observed_nodes, 'discrete', discrete_nodes );
 
+p = 2;
 for i=1:N
     if i == 1 || i == 2 || i == 4 || i == 5 || i == 7 || i == 8 || ...
        i == 10 || i == 11 
         bnet.CPD{i} = root_CPD(bnet, i);
     else 
+        k = node_sizes(i); 
+        ps = parents(dag, i); 
+        psz = prod(node_sizes(ps)); 
+        CPT = sample_dirichlet(p*ones(1,k), psz);
 
         %if i == 3
         %    node_size = preflop_buckets;
@@ -45,8 +51,9 @@ for i=1:N
         %    node_size = river_buckets;
         %end
         %CPT = reshape( repmat( priors(i,:), node_size^2, 1 ), 1, active_action_state_size*node_size^2 );
-        bnet.CPD{i} = tabular_CPD(bnet, i);
+        bnet.CPD{i} = tabular_CPD(bnet, i, 'CPT', CPT);
     end
+end
 
 bnet_learned = learn_params(bnet, evidence);
 
